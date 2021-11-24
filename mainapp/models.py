14406332515f5f1e4158e4d7bef9d7d13b1b09f4
nezl0i <nezl0in@gmail.com>
2021-11-24 07:35:57 +0300

    create migrations from models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products_img', blank=True, null=True)
+    short_desc = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.name} <> {self.category.name}'
